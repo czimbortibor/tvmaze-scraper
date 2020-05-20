706bd470fde6402884e13da66b48df254f83b86e
Tibor Czimbor <czimbortibor@gmail.com>
2020-05-20 18:19:42 +0200

    add logging in the webscraper implementation

--- src/Infrastructure.Scraper/Scraper.cs
+++ src/Infrastructure.Scraper/Scraper.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using TvMazeScraper.Application.Common.Interfaces;
@@ -15,15 +16,17 @@ namespace Infrastructure.Scraper
     public class Scraper : IScraper
     {
         private readonly HttpClient _httpClient;
+        private readonly ILogger<Scraper> _logger;
         private readonly IApplicationDbContext _dbContext;
 
         private const string BaseScrapingUrl = "https://api.tvmaze.com/shows";
         private const string ScrapingUrlQueryParams = "?embed=cast";
         private readonly JsonSerializerSettings _jsonSerializerSettings;
 
-        public Scraper(HttpClient httpClient, IApplicationDbContext dbContext)
+        public Scraper(HttpClient httpClient, ILogger<Scraper> logger, IApplicationDbContext dbContext)
         {
             _httpClient = httpClient;
+            _logger = logger;
             _dbContext = dbContext;
 
             _jsonSerializerSettings = new JsonSerializerSettings
@@ -52,14 +55,25 @@ namespace Infrastructure.Scraper
         private async Task<TvMazeShowModel> Scrape(int index, CancellationToken cancellationToken)
         {
             string scrapingUrl = BuildNextResourceUrl(index);
-            HttpResponseMessage response = await _httpClient.GetAsync(scrapingUrl, cancellationToken);
 
-            response.EnsureSuccessStatusCode();
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(scrapingUrl, cancellationToken);
 
-            string responseContent = await response.Content.ReadAsStringAsync();
-            TvMazeShowModel tvShowModel = GetModel(responseContent);
+                response.EnsureSuccessStatusCode();
 
-            return tvShowModel;
+                string responseContent = await response.Content.ReadAsStringAsync();
+                TvMazeShowModel tvShowModel = GetModel(responseContent);
+
+                return tvShowModel;
+            }
+            catch (Exception ex)
+            {
+                // ignore for now
+                
+                _logger.LogError(ex, ex.Message);
+                return null;
+            }
         }
 
         private TvMazeShowModel GetModel(string rawData)
